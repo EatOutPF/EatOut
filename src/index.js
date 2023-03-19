c38f299f6000ada9f5ca02afa3413c84c383f1c7
Claudio <cditoro@gmail.com>
2023-03-19 12:46:51 -0300

    prueba de santi

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const Restaurant = require("./Models/Restaurant");
+const Menu = require("./Models/Menu");
+const Diet = require("./Models/Diet");
+const PaymentMethod = require("./Models/PaymentMethod");
+const Atmosphere = require("./Models/Atmosphere");
+const Extra = require("./Models/Extra");
+const Room = require("./Models/Room");
+const { ATLAS_URI, PORT } = process.env;
+const mongoose = require("mongoose");
+
+
+mongoose
+  .connect(ATLAS_URI, {
+    maxPoolSize: 50,
+    waitQueueTimeoutMS: 2500,
+    useNewUrlParser: true,
+  })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
+
+module.exports = {
+  Restaurant,
+  Menu,
+  Diet,
+  PaymentMethod,
+  Atmosphere,
+  Extra,
+  Room,
+};
